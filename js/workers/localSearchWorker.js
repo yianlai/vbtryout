d717594cc62faa59f51f40e4b7b3908c521361e3
Morgan Lai <nagrom42@gmail.com>
2016-11-22 16:03:38 -0800

    tryout

---
+++ js/workers/localSearchWorker.js
@@ -0,0 +1,11 @@
+(function() {
+    "use strict";
+
+    importScripts('../lib/fuse.min.js', 'localSearchHelper.js');
+
+    self.onmessage = function(e) {
+        var results = localSearchHelper.localTranscriptSearch(e.data.transcript, e.data.terms, e.data.isApi2_0);
+        self.postMessage({results: results});
+    };
+
+})();
